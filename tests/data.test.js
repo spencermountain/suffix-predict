f6b90738c0279a59ae274bb696aa664d468e4afe
spencer kelly <spencerkelly86@gmail.com>
2022-02-13 16:46:36 -0500

    more tests - working well

--- tests/data.test.js
+++ tests/data.test.js
@@ -1,11 +1,14 @@
 import test from 'tape'
 import { classify, predict } from '../src/index.js'
-import data from './data.js'
+import misc from './data/misc.js'
+import frNous from './data/fr-nous.js'
+import frWords from './data/fr-words.js'
+import futureSimple from './data/future-simple.js'
 
-test('data', function (t) {
-  let model = classify(data)
-  Object.keys(data).forEach(tag => {
-    let pairs = data[tag]
+test('misc', function (t) {
+  let model = classify(misc)
+  Object.keys(misc).forEach(tag => {
+    let pairs = misc[tag]
     pairs.forEach(str => {
       let found = predict(str, model)
       t.equal(found, tag, str + ' : ' + found)
@@ -13,3 +16,49 @@ test('data', function (t) {
   })
   t.end()
 })
+
+test('frNous', function (t) {
+  let pairs = []
+  frNous.forEach(a => {
+    pairs.push([a[0], 'En'])
+    pairs.push([a[1], 'Fr'])
+  })
+  let model = classify(pairs)
+  pairs.forEach(a => {
+    let [str, tag] = a
+    let found = predict(str, model)
+    t.equal(found, tag, str + ' : ' + found)
+  })
+  t.end()
+})
+
+test('frWords', function (t) {
+  let pairs = []
+  frWords.forEach(a => {
+    pairs.push([a[0], 'Fr'])
+    pairs.push([a[1], 'En'])
+  })
+  let model = classify(pairs)
+  pairs.forEach(a => {
+    let [str, tag] = a
+    let found = predict(str, model)
+    t.equal(found, tag, str + ' : ' + found)
+  })
+  t.end()
+})
+
+test('futureSimple', function (t) {
+  let pairs = []
+  futureSimple.forEach(a => {
+    pairs.push([a[0], 'Pres'])
+    pairs.push([a[1], 'Fut'])
+  })
+  let model = classify(pairs)
+  pairs.forEach(a => {
+    let [str, tag] = a
+    let found = predict(str, model)
+    t.equal(found, tag, str + ' : ' + found)
+  })
+  t.end()
+})
+
